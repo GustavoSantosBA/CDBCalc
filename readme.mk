860d4a80922a3c0ccd8920ae3ab17e59f7c52175
Gustavo Santos <gugasoftba@gmail.com>
2023-03-01 20:54:11 -0300

    .

---
+++ readme.mk
@@ -0,0 +1,8 @@
+Instalação
+1 - Para executar o projeto Angular, é necessário rodar o comando npm install
+
+Testes
+1 - Informar um valor negativo para o campo Valor presente e clicar no botão calcular
+2 - Informar um valor negativo para o período e clicar no botão calcular
+3 - Não informar nenhum valor e nenhum dos campos e clicar em calcular
+4 - Informar um valor maior que 1 para o mês e um valor maior que 0 para o valor presente e clicar em calcular
